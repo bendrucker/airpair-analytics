ee82f9dabbb8c54ad2b8325b4ef6da4b3ca379ae
Ben Drucker <bvdrucker@gmail.com>
2014-10-15 16:47:02 -0400

    Update total visitors in realtime

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,11 @@
 (function (Firebase) {
   var analytics = new Firebase('https://airpair-analytics-tutorial.firebaseio.com/');
 
+  $(document).on('ready', function () {
+    $totalVisitors = $('#total-visitors');
+    analytics.child('totalVisitors').on('value', function (snapshot) {
+      $totalVisitors.text(snapshot.val());
+    });
+  });
 
 })(Firebase);
