be378b38aed2b911318c4c1a618557d60cbf86cf
Ben Drucker <bvdrucker@gmail.com>
2014-10-15 17:19:40 -0400

    Display active visitors when added

--- dashboard.js
+++ dashboard.js
@@ -3,9 +3,27 @@
 
   $(document).on('ready', function () {
     var $totalVisitors = $('#total-visitors');
+    var $activeVisitors = $('#active-visitors');
+
     analytics.child('totalVisitors').on('value', function (snapshot) {
       $totalVisitors.text(snapshot.val());
     });
+
+    var activeVisitors = analytics.child('activeVisitors');
+    activeVisitors.on('child_added', function (snapshot) {
+      var n = snapshot.name();
+      var v = snapshot.val();
+      $activeVisitors.prepend(
+      '<li id="active-visitor' + n + '">' + n + ':' +
+        '<ul>' +
+          '<li>Arrived: ' + new Date(v.arrivedAt) + '</li>' +
+          '<li>Path: ' + v.path + '</li>' +
+          '<li>User Agent: ' + v.userAgent + '</li>' +
+        '</ul>' + 
+      '</li>'
+      );
+    });
+
   });
 
 })(Firebase);
