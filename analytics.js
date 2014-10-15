c0d078baee2867c7e6786a8745ab9d5f7f8754fc
Ben Drucker <bvdrucker@gmail.com>
2014-10-15 15:41:09 -0400

    Add support for adding past visitors on disconnect

--- analytics.js
+++ analytics.js
@@ -2,12 +2,21 @@
   var analytics = new Firebase('https://airpair-analytics-tutorial.firebaseio.com/');
   var activeVisitors = analytics.child('activeVisitors');
 
-  activeVisitors.push({
+  var visitor = {
     path: window.location.pathname,
     arrivedAt: Firebase.ServerValue.TIMESTAMP,
     userAgent: navigator.userAgent
-  });
+  };
 
+  var visitorId = activeVisitors.push(visitor, function () {
+    activeVisitors.child(visitorId).once('value', function (snapshot) {
+      visitor.arrivedAt = snapshot.val().arrivedAt;
+    });
+  }).name();
+  
+  var pastVisitors = analytics.child('pastVisitors');
+  visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
+  pastVisitors.child(visitorId).onDisconnect().set(visitor);
 
   var totalVisitors = analytics.child('totalVisitors');
   totalVisitors.once('value', function (snapshot) {
